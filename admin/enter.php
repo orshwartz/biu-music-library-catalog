01ab8e5b68eb5760f18d96399a46f082ae2a3702
shwartz@slimail.com <shwartz@slimail.com@8f86897b-49f2-6f80-f085-0bb4fbfb7bc8>
2011-04-30 21:49:36 +0000

    Appearance: Chage background color

--- admin/enter.php
+++ admin/enter.php
@@ -9,50 +9,19 @@ include_once('../styles.inc');
 <head>
 	<title>מערכת הזנת נתונים</title>
 	<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1255">
-
 </head>
 
 <body>
 <center>
 <!-- displays the enter page -->
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%">
-   <tr>
-       <td align=center><img src="../images/blank.gif"></td>
-       <td class="enterTitle1" align=center valign=center >אוניברסיטת בר-אילן</td>
-       <td align=center><img src="../images/blank.gif"></td>
-
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="../images/blank.gif"></td>
-       <td class="enterTitle2" align=center valign=center >הספריה למוזיקה</td>
-       <td align=center><img src="../images/blank.gif"></td>
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="../images/blank.gif"></td>
-       <td class="enterTitle3" align=center valign=center><br></td>
-       <td align=center><img src="../images/blank.gif"></td>
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="../images/blank.gif"></td>
-       <td class="enterTitle3" align=center valign=center>קטלוג מדיה</td>
-       <td align=center><img src="../images/blank.gif"></td>
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="../images/blank.gif"></td>
-       <td class="enterTitle4" align=center valign=center>מערכת הזנת נתונים</td>
-       <td align=center><img src="../images/blank.gif"></td>
-   </tr>
-</table>
-
+<br>
+<div class="enterTitle1" align=center valign=center >אוניברסיטת בר-אילן</div>
+<div class="enterTitle2" align=center valign=center >הספריה למוזיקה</div>
+<br>
+<div class="enterTitle3" align=center valign=center>קטלוג מדיה</div>
+<div class="enterTitle4" align=center valign=center>מערכת הזנת נתונים</div>
 <br><br>
+
 <!-- the button links to the select operation page -->
 <form action="selectOperation.php"><input alt="כניסה" type="image" src="../images/enter.gif"></form>
 </center>
--- enter.php
+++ enter.php
@@ -14,44 +14,14 @@ include_once('styles.inc');
 <body>
 <center>
 <!-- displays the enter page -->
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%">
-   <tr>
-       <td align=center><img src="images/blank.gif"></td>
-       <td class="enterTitle1" align=center valign=center >אוניברסיטת בר-אילן</td>
-       <td align=center><img src="images/blank.gif"></td>
-
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="images/blank.gif"></td>
-       <td class="enterTitle2" align=center valign=center >הספריה למוזיקה</td>
-       <td align=center><img src="images/blank.gif"></td>
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="images/blank.gif"></td>
-       <td class="enterTitle3" align=center valign=center><br></td>
-       <td align=center><img src="images/blank.gif"></td>
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="images/blank.gif"></td>
-       <td class="enterTitle3" align=center valign=center>קטלוג מדיה</td>
-       <td align=center><img src="images/blank.gif"></td>
-   </tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 bordercolor=black align=center width="100%"">
-   <tr>
-       <td align=center><img src="images/blank.gif"></td>
-       <td class="enterTitle4" align=center valign=center>מערכת חיפוש נתונים</td>
-       <td align=center><img src="images/blank.gif"></td>
-   </tr>
-</table>
-
+<br>
+<div class="enterTitle1" align=center valign=center >אוניברסיטת בר-אילן</div>
+<div class="enterTitle2" align=center valign=center >הספריה למוזיקה</div>
+<br>
+<div class="enterTitle3" align=center valign=center>קטלוג מדיה</div>
+<div class="enterTitle4" align=center valign=center>מערכת חיפוש נתונים</div>
 <br><br>
+
 <!-- the button links to the simple search page -->
 <form action="search.php"><input alt="כניסה" type="image" src="images/enter.gif"></form>
 </center>
