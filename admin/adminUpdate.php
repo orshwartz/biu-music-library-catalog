31d689f4ffff0cc51931de9dee65787f5d12507e
shwartz@slimail.com <shwartz@slimail.com@8f86897b-49f2-6f80-f085-0bb4fbfb7bc8>
2011-04-30 21:49:59 +0000

    Remove the "back" and "forward" buttons from the bottoms of the search pages (it's irrelevant there).

--- admin/adminUpdate.php
+++ admin/adminUpdate.php
@@ -773,9 +773,6 @@ if (!isset($mode)) {
 <center>
 <br><br>
 <hr width=500>
-<br><br>
-<!-- back and forward navigation arrows -->
-<a href="javascript:history.forward();"><img src="../images/forward_arrow_ani.gif" alt="לעמוד הבא" border=0></a><img src="../images/blank.gif"><a href="javascript:history.back();"><img src="../images/back_arrow_ani.gif" alt="לעמוד הקודם" border=0></a>
 </center>
 
 </body>
