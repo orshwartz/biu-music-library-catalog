fb84159b55788bab22da795821e0879098b9e423
shwarto <shwarto@8f86897b-49f2-6f80-f085-0bb4fbfb7bc8>
2011-05-31 08:31:22 +0000

    Improved (hopefully) language detection for results display.

--- results.php
+++ results.php
@@ -28,11 +28,30 @@ include_once('lang.php') ;
 // the alignment should be set according to this.
 function determineLang($field)
 {
-    if ((ord(substr($field, 0, 1)) >= 0) && (ord(substr($field, 0, 1)) <= 127)) {
-        return "en";
-    } else {
-        return "heb";
-    }
+    // Reset non-standard character counter
+	$non_standard_char_count = 0;
+	
+	// For each character in string
+	$field_len = strlen($field);
+	for ($cur_char_idx = 0; $cur_char_idx < $field_len; ++$cur_char_idx)
+	{
+		// If this is a non-standard ASCII code (meaning, extended)
+		if (127 < ord($field[$cur_char_idx]))
+		{
+			// Increase non-standard char count
+			$non_standard_char_count++;
+		}
+		
+		// If found at-least one non-standard code
+		if ($non_standard_char_count > 0)
+		{
+			// Assume Hebrew text
+			return "heb";
+		}
+	}
+	
+	// Assume English text
+	return "en";
 }
 
 if (!isset($_GET['mode']))
